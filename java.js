5c908c02e8db647852506aec8a7d2bdb61a021fd
flecha <jonathanricardomachado@gmail.com>
2024-06-11 21:48:11 -0300

    debungando API

--- java.js
+++ java.js
@@ -1,13 +1,39 @@
 async function getAddressByCep(){
     const cep = document.getElementById('cep').value;
+
     try {
         const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         const data = await response.json();
-        console.log(data.cep);
-        document.getElementById('logradouro').value.innerText = data.logradouro;
-        document.getElementById('bairro').value.innerText = data.bairro;
-        document.getElementById('localidade').value.innerText = data.uf;
+
+        console.log(data);
+        // const log =;
+        const brr = data.bairro;
+        const loc = data.localidade;
+
+        document.getElementById('log').innerText = data.logradouro != "" ? data.logradouro : "--";
+        document.getElementById('brr').innerText = data.bairro != "" ? data.bairro :  "--";
+        document.getElementById('loc').innerText = data.localidade;
+
     } catch (error) {
-        alert("Digite um cep valido!");
+        alert(error.message);
+    }
+}
+
+async function getAddressByMeteo(){
+    const lat = document.getElementById(`lat`).value;
+    const long = document.getElementById(`long`).value;
+    
+    console.log(lat);
+    console.log(long);
+
+    try {
+        const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${long}&current=temperature_2m`);
+        const data = await response.json();
+
+        const temp = data.current.temperature_2m;
+        
+        document.getElementById(`temp`).innerText = `Previsão de tempo de acordo com a região: ${temp}°C`;
+    } catch (error) {
+        alert(error.message);
     }
 }
